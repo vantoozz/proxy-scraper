85091618b18ed0ac3f43609111faa3e88a015381
Ivan Nikitin <vantoozz@gmail.com>
2017-09-08 14:40:03 +0300

    ~ scrapers updated

---
+++ src/Text.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+
+namespace Vantoozz\ProxyScraper;
+
+final class String
+{
+}
---
+++ tests/unit/TextTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace Vantoozz\ProxyScraper;
+
+use PHPUnit\Framework\TestCase;
+
+final class TextTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_works(): void
+    {
+        $this->assertTrue(true);
+    }
+}
