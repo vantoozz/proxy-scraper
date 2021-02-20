402e0beebbb8bb3cf2cd2b99325cc73d73dcaac0
Ivan Nikitin <vantoozz@gmail.com>
2021-02-20 08:44:30 -0500

    ~ example

--- examples/07-metrics.php
+++ examples/07-metrics.php
@@ -1,17 +1,11 @@
 <?php declare(strict_types=1);
 
-use Vantoozz\ProxyScraper\Proxy;
-use Vantoozz\ProxyScraper\Scrapers\FreeProxyListScraper;
+use Vantoozz\ProxyScraper\Scrapers;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$httpClient = guzzleHttpClient();
+$scraper = new Scrapers\FreeProxyListScraper(guzzleHttpClient());
 
-$scraper = new FreeProxyListScraper($httpClient);
-
-/** @var Proxy $proxy */
-$proxy = $scraper->get()->current();
-
-foreach ($proxy->getMetrics() as $metric) {
-    echo $metric->getName() . ': ' . $metric->getValue() . "\n";
+foreach ($scraper->get() as $proxy) {
+    echo $proxy . "\n";
 }
