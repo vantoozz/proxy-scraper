ca71108e5a91bf39995194989b8b832ada3afff7
Ivan Nikitin <vantoozz@gmail.com>
2021-02-20 08:39:31 -0500

    ~ example

--- examples/07-metrics.php
+++ examples/07-metrics.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 
 use Vantoozz\ProxyScraper\Proxy;
-use Vantoozz\ProxyScraper\Scrapers;
+use Vantoozz\ProxyScraper\Scrapers\FreeProxyListScraper;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $httpClient = guzzleHttpClient();
 
-$scraper = new Scrapers\BlogSpotProxyScraper($httpClient);
+$scraper = new FreeProxyListScraper($httpClient);
 
 /** @var Proxy $proxy */
 $proxy = $scraper->get()->current();
